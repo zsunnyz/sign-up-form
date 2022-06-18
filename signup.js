22cb6373452240b6420ab1491eb6a263f1542843
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-18 13:51:55 +1000

    added password same validation

--- signup.js
+++ signup.js
@@ -1,10 +1,17 @@
 const passwordInput = document.getElementById("password");
 const confirmPasswordInput = document.getElementById("confirm-password");
+const passwordError = document.getElementById("password-error")
 
 confirmPasswordInput.addEventListener('input', (e)=>{
     if (passwordInput.value != "" &&  e.target.value != passwordInput.value){
-        passwordInput.style.outline = "1px solid red";
-        confirmPasswordInput.style.outline = "1px solid red";
+        passwordInput.style.border = "1px solid red";
+        confirmPasswordInput.style.border = "1px solid red";
+        passwordError.style.visibility = "visible";
+    }
+    else if(passwordInput.value == e.target.value) {
+        passwordInput.style.border = "1px solid lightgrey";
+        confirmPasswordInput.style.border = "1px solid lightgrey";
+        passwordError.style.visibility = "hidden";
         
     }
 })
